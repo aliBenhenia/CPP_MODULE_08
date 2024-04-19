3a8ecf4de731db08374e6b12dac05cdd8ee20253
Ali ben <alibenhenia1@gmail.com>
2024-04-19 13:10:50 +0100

    dd

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abenheni <abenheni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 21:54:30 by abenheni          #+#    #+#             */
-/*   Updated: 2024/03/27 22:59:06 by abenheni         ###   ########.fr       */
+/*   Updated: 2024/04/19 11:48:18 by abenheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@
 
  int main()
  {
-        MutantStack<int> mstack;
-        mstack.push(5);
-        mstack.push(17);
-        std::cout << mstack.top() << std::endl;
-        mstack.pop();
-        std::cout << mstack.size() << std::endl;
-        mstack.push(3);
-        mstack.push(5);
-        mstack.push(737);
-        //[...]
-        mstack.push(0);
-        MutantStack<int>::iterator it = mstack.begin();
-        MutantStack<int>::iterator ite = mstack.end();
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
         ++it;
-        --it;
-        while (it != ite)
-        {
-            std::cout << *it << std::endl;
-            ++it;
-        }
-        std::stack<int> s(mstack);
-        return 0;
+    }
+    std::stack<int> s(mstack);
+    return 0;
  }
