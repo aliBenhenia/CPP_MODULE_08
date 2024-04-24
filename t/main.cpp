a5d48c4a3ddff5b6e56fc73ca1437c4998044619
Ali ben <alibenhenia1@gmail.com>
2024-04-24 11:05:42 +0100

    UPDATE

--- t/main.cpp
+++ t/main.cpp
@@ -1,24 +1,30 @@
+#include <deque>
 #include <iostream>
-#include <vector>
 
-template <typename T>
-typename T::iterator geet(T obj)
+void print(std::deque<int> copy)
 {
-    return obj.begin();
+    std::deque<int>::iterator it = copy.begin();
+    while (it != copy.end())
+    {
+        std::cout << *it << std::endl;
+        it++;
+    }
+    std::cout << "*********************************************" << std::endl;
+    
 }
-
 int main()
 {
-    // int arr[] = {9,1,2,3,4,5,6,7,8,2,2,2,4,3,4};
-    // std::sort(arr, arr + 3);
-    // for(int i = 0; i < 6 ;i++)
-    // {
-    //     std::cout << arr[i] << std::endl;
-    // }
+    std::deque<int> data;
 
-    std::vector<int>::iterator v = geet(std::vector<int>({1,4,45,5}));
-
-    std::cout << v[3];
-
+    data.push_back(1); 
+    data.push_back(2); 
+    data.push_back(3);
+    print(data);
+    data.push_front(-1);
+    data.push_front(-2);
+    data.push_front(-3);
+    print(data);
+    data.pop_back();
+    print(data);
     return 0;
 }
